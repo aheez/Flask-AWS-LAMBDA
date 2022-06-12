f14391c8ec0057880050f6e823601c12ac880137
aheez <a.almofeez94@gmail.com>
2022-06-12 16:54:23 +0300

    misc

--- src/app.py
+++ src/app.py
@@ -5,6 +5,7 @@ priority_list = ["H", "M", "L"]
 NO_LOCATIONS = 10
 
 mainWH = location(longitude=26.305201, latitude=50.149044, priority="L", idx=0)
+mainWH_gmaps = gmaps.geocode(mainWH.longitude, mainWH.latitude)
 
 MAX = (26.463428, 50.184721)
 MIN = (26.273890, 50.025944)
@@ -12,15 +13,30 @@ MIN = (26.273890, 50.025944)
 maximum = (1e4 * (MAX[0] - MIN[0]))
 minimum = (1e4 * (MAX[1] - MIN[1]))
 
-temp_long = [random.randint(10, int(maximum)) / 1e4 for x in range(NO_LOCATIONS)]
-temp_lati = [random.randint(10, int(minimum)) / 1e4 for x in range(NO_LOCATIONS)]
+temp_long = [random.randint(10, int(maximum)) / 1e4 + MIN[0] for x in range(NO_LOCATIONS)]
+temp_lati = [random.randint(10, int(minimum)) / 1e4 + MIN[1] for x in range(NO_LOCATIONS)]
 
 test_locations = [location(longitude=temp_long[i], latitude=temp_lati[i], priority=priority_list[random.randint(0, 2)], idx= i + 1) for i in range(NO_LOCATIONS)]
 
 for i in range(len(test_locations)):
 	test_locations[i].set_weight(distance(mainWH, test_locations[i]))
-	print(test_locations[i].weight)
+# 	print(test_locations[i].weight)
+
+# map(lambda x: x.set_weight(distance(mainWH, x)), test_locations)
+print_locations(test_locations)
 
 # TODO: Sort them
+# sorted_set = sort_locations(test_locations)
+sorted_set = sorted(test_locations, key=lambda x: x.weight)
+# temp = [x.weight for x in test_locations]
+# temp = temp.sort()
+# for i in range(len(temp)):
+# 	for j in range(len(test_locations)):
+# 		if (test_locations[j].weight == temp[i]):
+# 			sorted_set.append(test_locations[j])
+# 			break
+
+print('\n\nSorted:')
+print_locations(sorted_set)
 
 # TODO: GOOGLE MAPS INTEGRATION
--- src/classes.py
+++ src/classes.py
@@ -1,4 +1,7 @@
 from math import sqrt
+import googlemaps, requests, json
+
+gmaps = googlemaps.Client(key=None)
 
 class location():
     def __init__(self, longitude: float, latitude: float, priority, idx: int):
@@ -6,20 +9,32 @@ class location():
         self.latitude   = latitude
         self.priority = self.set_priority(priority)
         self.id = idx
+        self.distance = 0xFF
         self.weight = 0xFFFFFFFF
 
     def set_priority(self, priority):
         if (priority == "H"):
-            return 1
+            return 10
         elif (priority == "M"):
-            return 2
+            return 20
         elif (priority == "L"):
-            return 3
+            return 30
         else:
-            return 3.5
+            return 35
     
     def set_weight(self, unscaled:float):
         self.weight = unscaled * self.priority
 
 def distance(start: location, end:location):
     return (sqrt((start.longitude - end.longitude) ** 2 + (start.latitude - end.latitude) ** 2))
+
+# TODO: GOOGLE MAPS INTEGRATION FOR DISTANCE
+# def get_distance(start: location, end:location):
+
+
+def sort_locations(locations: list):
+    return locations.sort(key=lambda x: x.weight)
+
+def print_locations(locations: list):
+    for i in range(len(locations)):
+        print(locations[i].id, '\t', locations[i].longitude, '\t\t', locations[i].latitude, '\t\t', locations[i].weight)
