ab4fea04ab847b98e30b5392173cb08f6a5da92d
Ahmad Almofeez <a.almofeez94@gmail.com>
2022-06-16 02:40:26 +0300

    Simple Huristic Search Algorithm Solution
    Partial implementation

--- src/app.py
+++ src/app.py
@@ -5,7 +5,8 @@ priority_list = ["H", "M", "L"]
 NO_LOCATIONS = 10
 
 mainWH = location(longitude=26.305201, latitude=50.149044, priority="L", idx=0)
-mainWH_gmaps = gmaps.geocode(mainWH.longitude, mainWH.latitude)
+# mainWH (26.305201, 50.149044)
+# mainWH_gmaps = gmaps.geocode(mainWH.longitude, mainWH.latitude)
 
 MAX = (26.463428, 50.184721)
 MIN = (26.273890, 50.025944)
@@ -16,18 +17,25 @@ minimum = (1e4 * (MAX[1] - MIN[1]))
 temp_long = [random.randint(10, int(maximum)) / 1e4 + MIN[0] for x in range(NO_LOCATIONS)]
 temp_lati = [random.randint(10, int(minimum)) / 1e4 + MIN[1] for x in range(NO_LOCATIONS)]
 
-test_locations = [location(longitude=temp_long[i], latitude=temp_lati[i], priority=priority_list[random.randint(0, 2)], idx= i + 1) for i in range(NO_LOCATIONS)]
+# test_locations = [location(longitude=temp_long[i], latitude=temp_lati[i], priority=priority_list[random.randint(0, 2)], idx= i + 1) for i in range(NO_LOCATIONS)]
+test_locations = [
+	(26.323264, 50.161992, 'L', 100),			# Test location 1 (Rayeg)
+	(26.334951, 50.191867, 'H', 200),
+	(26.373389, 50.236519, 'M', 300)			# 5751 نفيس بن هلال،, الصدفة، 8750, الخبر 34215
+]
 
-for i in range(len(test_locations)):
-	test_locations[i].set_weight(distance(mainWH, test_locations[i]))
+test_locations = [location(x[0], x[1], x[2], x[3]) for x in test_locations]
+
+# for i in range(len(test_locations)):
+# 	test_locations[i].set_distance(distance(mainWH, test_locations[i]))
 # 	print(test_locations[i].weight)
 
-# map(lambda x: x.set_weight(distance(mainWH, x)), test_locations)
-print_locations(test_locations)
+# map(lambda x: x.set_distance(distance(mainWH, x)), test_locations)
+# print_locations(test_locations)
 
 # TODO: Sort them
 # sorted_set = sort_locations(test_locations)
-sorted_set = sorted(test_locations, key=lambda x: x.weight)
+# sorted_set = sorted(test_locations, key=lambda x: x.cost)
 # temp = [x.weight for x in test_locations]
 # temp = temp.sort()
 # for i in range(len(temp)):
@@ -36,7 +44,17 @@ sorted_set = sorted(test_locations, key=lambda x: x.weight)
 # 			sorted_set.append(test_locations[j])
 # 			break
 
+# new_order = [] 
+# for i in range(len(test_locations)):
+# 	new_order.append(sorted_set.pop())
+# 	map(lambda x: x.set_distance(distance(new_order[i],x)), sorted_set)
+# 	sorted_set = sorted(sorted_set, key=lambda x: x.cost)
+# 	print("\n", i, "Pass:")
+# 	print_locations(sorted_set)
+
+route = gen_route(mainWH, test_locations)
 print('\n\nSorted:')
-print_locations(sorted_set)
+# print_locations(sorted_set)
+print_locations(route)
 
 # TODO: GOOGLE MAPS INTEGRATION
--- src/classes.py
+++ src/classes.py
@@ -2,7 +2,8 @@ from math import sqrt
 import googlemaps, requests, json
 from googlemaps import distance_matrix
 
-gmaps = googlemaps.Client(key=None)
+DEBUG = True
+# gmaps = googlemaps.Client(key=None)
 
 class location():
     def __init__(self, longitude: float, latitude: float, priority, idx: int):
@@ -10,8 +11,8 @@ class location():
         self.latitude   = latitude
         self.priority = self.set_priority(priority)
         self.id = idx
-        self.distance = 0xFFFF
-        self.weight = 0xFFFFFFFF
+        self.distance = 0
+        self.cost = 0
 
     def set_priority(self, priority):
         if (priority == "H"):
@@ -24,11 +25,11 @@ class location():
             return 3.5
     
     def set_distance(self, new_distance):
-        self.distance = new_distance
-        self.set_weight(new_distance)
+        self.distance = new_distance * 100
+        self.set_cost(new_distance)
 
-    def set_weight(self, unscaled:float):
-        self.weight = unscaled * self.priority
+    def set_cost(self, weight:float):
+        self.cost = weight * self.priority
     
     def get_coordinates(self):
         return (self.latitude, self.longitude)
@@ -38,19 +39,42 @@ def distance(start: location, end:location):
 
 # TODO: GOOGLE MAPS INTEGRATION FOR DISTANCE
 def get_distance(start: location, end:list):
-    end_list = [x.get_coordinates for x in end]
-    response = distance_matrix(gmaps, start.get_coordinates(), end_list, mode="driving", units="metric")
-    distances = [response['rows'][0]['elements'][x]['distance']['value'] for x in range(len(response['rows'][0]['elements']))]
-    
-    if len(distances) != len(end):
-        raise ValueError("API responce error")
+    if DEBUG:
+        end_list = [x.get_coordinates for x in end]
+        # response = distance_matrix(gmaps, start.get_coordinates(), end_list, mode="driving", units="metric")
+        response= json.JSONDecoder('./RES_SAMPLE.json')
+        distances = [response['rows'][0]['elements'][x]['distance']['value'] for x in range(len(response['rows'][0]['elements']))]
+        
+        if len(distances) != len(end):
+            raise ValueError("API responce error")
+        else:
+            for i in range(len(end)):
+                end[i].set_distance(distances[i])
     else:
-        for i in range(len(end)):
-            end[i].set_distance = distances[i]
+        for location in end:
+            location.set_distance(distance(start, location))
+
+def gen_route(start:location, locations: list) -> list:
+    sorted_list = update_cost(start, locations)
+    route = []
+    route.append(start)
+    
+    for i in range(len(sorted_list)):
+        sorted_list = update_cost(route[i], sorted_list)
+        route.append(sorted_list.pop())
+    
+    if len(route) != len(locations) + 1:
+        print("Err")
+    
+    return route
+
+def update_cost(start:location, end: list) -> list:
+    map(lambda x: x.set_distance(get_distance(start, end)), end)
+    return sorted(end, key=lambda x: x.cost)
 
 def sort_locations(locations: list):
     return locations.sort(key=lambda x: x.weight)
 
 def print_locations(locations: list):
     for i in range(len(locations)):
-        print(locations[i].id, '\t', locations[i].longitude, '\t\t', locations[i].latitude, '\t\t', locations[i].weight)
+        print(locations[i].id, '\t', locations[i].longitude, '\t\t', locations[i].latitude, '\t\t', locations[i].distance)
